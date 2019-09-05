0fa2b2c13e75a301b07f3964314108953be372fc
Aftab Shaikh <aftabshk@In-Aftabshk.local>
2019-09-06 01:14:00 +0530

    Changed selector for app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "promo-comm",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-  title = 'promo-comm';
+  title = "promo-comm";
 }
